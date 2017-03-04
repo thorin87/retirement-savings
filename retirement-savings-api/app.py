fd20f25028be14583f5ef5ad7ca72cb3dfaa4c19
Daniel Dymek <dymekdaniel@gmail.com>
2017-03-04 18:52:27 +0100

    [api] dodana metoda zwracająca statyczny json z danymi do przykładowego wykresu i plik z tymi danymi

--- retirement-savings-api/app.py
+++ retirement-savings-api/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask import Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return "Hello World!"
+	
+@app.route("/test")
+def testdata():
+    file = open('static/testdata.json', 'r') 
+    file_contents = file.read()
+    resp = Response(file_contents, status=200, mimetype='application/json')
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
