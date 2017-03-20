116bcdca8c246c2d86521fdede18cc285e0649fb
Daniel Dymek <dymekdaniel@gmail.com>
2017-03-20 15:58:10 +0100

    dodane mapowania decimal do json, próba kontroli błędów, usuniete predefiniowane dane do wykresu

--- retirement-savings-api/app.py
+++ retirement-savings-api/app.py
@@ -4,24 +4,18 @@ from flask import jsonify
 from flask_cors import CORS, cross_origin
 from flaskext.mysql import MySQL
 
-def decimal_default(obj):
-    if isinstance(obj, decimal.Decimal):
-        return float(obj)
-    raise TypeError
+import decimal
+import flask.json
 
-'''
-class CustomJSONEncoder(JSONEncoder):
+class MyJSONEncoder(flask.json.JSONEncoder):
     def default(self, obj):
-        try:
         if isinstance(obj, decimal.Decimal):
-            return float(obj)
-        except TypeError:
-            pass
-        else:
-            return list(iterable)
-        return JSONEncoder.default(self, obj)
-'''
+            # Convert decimal instances to strings.
+            return str(obj)
+        return super(MyJSONEncoder, self).default(obj)
+
 app = Flask(__name__)
+app.json_encoder = MyJSONEncoder
 CORS(app)
 #app.json_encoder = CustomJSONEncoder
 mysql = MySQL()
@@ -53,6 +47,16 @@ def fetchfromdb():
     query_string = "SELECT Date, CAST(VALUE * 100 AS INT) FROM rate WHERE Date > '2015-01-01' AND FundId = 1" 
     cursor.execute(query_string)
     return jsonify(data=cursor.fetchall())
+
+@app.route("/dbtest_errorhandling")
+def fetchfromdberrorhandling():
+    query_string = "SELECT Date, Value FROM rate WHERE Date > '2015-01-01' AND FundId = 1" 
+    try:
+        cursor.execute(query_string)
+        data = cursor.fetchall()
+    except:
+        data = "Error: unable to fetch items"
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- retirement-savings-ui/src/app/chart/chart.component.ts
+++ retirement-savings-ui/src/app/chart/chart.component.ts
@@ -16,14 +16,9 @@ export class ChartComponent implements OnInit {
     }
   }
 
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-public lineChartType = "line";
+  public lineChartData:Array<any>;
+  public lineChartLabels:Array<any>;
+  public lineChartType = "line";
 
   constructor() { }
 
